1b8d6e9f11b12f2c3f7b150c9c78c176ef3cb1f3
sachin pargai <pargaisachin11@gmail.com>
2024-01-07 13:01:02 +0530

    Tailwind and react router dom installed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-cyan-800 font-bold font-extrabold`'> 
+      I am Here to build netflix GPT
+    </div>
+  )
+}
+
+export default App
